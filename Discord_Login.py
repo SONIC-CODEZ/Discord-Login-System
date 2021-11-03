995f6a628302094fb9310d620193344c05c13cdc
SONIC-CODEZ <84275414+SONIC-CODEZ@users.noreply.github.com>
2021-11-03 15:08:02 +0000

    Update Discord_Login.py

--- Discord_Login.py
+++ Discord_Login.py
@@ -1,17 +1,20 @@
 import random
 import time
 
-
 class Discord:
     def __init__(self, username, password, age, email):
         self.username = username
         self.password = password
         self.age = age
         self.email = email
-
+        
+#c
     def checkAge(self):
         if int(self.age) < 13:
             exit("Underage!")
+
+        elif '@gmail.com' in self.email or '@ymail.com' in self.email:
+            print('Updated!')
 
     def checkPassword(self):
         if int(self.password) > 100:
@@ -24,6 +27,7 @@ class Discord:
 if __name__ == '__main__':
     while True:
         # USERNAME
+        print("----------------------------------------------")
         input_for_username = input('Enter a username: ')
         arukovic = Discord(input_for_username, 143, '467', 'abc@gmail.com')
         arukovic.username = input_for_username
@@ -31,20 +35,23 @@ if __name__ == '__main__':
         time.sleep(2)
         print(arukovic.formatUsername())
         # PASSWORD
+        print("----------------------------------------------")
         time.sleep(1)
         input_for_password = input('Enter your password: ')
         arukovic.password = input_for_password
         arukovic.checkPassword()
         # AGE
+        print("----------------------------------------------")
         time.sleep(1)
         input_for_age = input('Enter your age: ')
         arukovic.age = input_for_age
         arukovic.checkAge()
         # EMAIL
+        print("----------------------------------------------")
         time.sleep(1)
         input_for_email = input('Enter your email: ')
         arukovic.email = input_for_email
-        # SHOW RESULTS
+        print("----------------------------------------------")
         print(f'Generating results @{arukovic.username}')
         time.sleep(2)
         print(f"Your username: {arukovic.username}, Your password: {arukovic.password}, Your age: {arukovic.age}, "
